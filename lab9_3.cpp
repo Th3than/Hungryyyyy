6720bd81b0ec2e0beb6f15bc87b208cf1a3458c3
Natan <Th3than@gmail.com>
2022-12-23 16:03:04 +0700

    lab 9-3

--- lab9_3.cpp
+++ lab9_3.cpp
@@ -1,13 +1,44 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include <iostream>
+using namespace std;
+int age,tall,bou;
+string cha;
+int main(){
+    cout << "Enter your age: ";
+    cin >> age;
+    if (age <= 25)
+    {
+        cout <<"Enter your height: ";
+        cin >> tall;
+        if (tall < 100)
+        {
+            cha = "Chopper";
+        }else if (tall <180)
+        {
+            cha = "Usopp";
+        }else{
+            cout <<"Enter your bounty: ";
+            cin >> bou;
+            if (bou > 1100000000)
+            {
+            cha = "Zoro";
+            }else{
+                cha = "Sanji";
+            }
+        }
+        
+    }else if (age <= 60)
+    {
+        cout <<"Enter your bounty: ";
+        cin >> bou;
+        if (bou > 500000000)
+        {
+            cha ="Jinbe";
+        }else{
+            cha ="Franky";
+        }
+        
+    }else{
+        cha = "Brook";
+    }
+    cout <<"Your character = "<<cha;
+}
